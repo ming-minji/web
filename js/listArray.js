d6c1a7a9247ca74db8e76d33a65149e106c7cc9f
김민지 <mando304010@gmail.com>
2026-03-23 15:05:04 +0900

    쌤 깃허브 보고 수정하기

---
+++ js/listArray.js
@@ -0,0 +1,83 @@
+let listArray = [
+    {
+        pid:0,
+        pName: "Grilled Sausage",
+        pDesc: "그릴에서 노릇하게 구워 육즙이 톡 터지는 소세지, 든든한 에너지가 필요한 당신을 위한 완벽한 선택",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:1,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:2,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:3,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:4,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:5,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:6,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:7,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    },
+    {
+        pid:8,
+        pName: "Sweet Corn Mayo",
+        pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
+        pPrice: 4500,
+        pThumnail: "list-sandwich.jpg",
+        // 썸네일에 들어갈 사진 만들기, 사진 저장할 땐 list-음식이름으로 저장하기. 저장된 이름 전체적으로 복사해서 썸네일 ""사이에 넣기
+        plike:false
+    }
+]
