efa5a7cf537d73ce2f909cd068c9332d14d875bf
김민지 <mando304010@gmail.com>
2026-03-30 11:53:09 +0900

    중간중간 안된 부분도 있음

--- script/header.js
+++ script/header.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",()=>{
+function header(){
 
     // 메뉴버튼을 누르면 header가 나오는 기능
     const btnMenu = document.querySelector('.btn-menu');
@@ -22,5 +22,4 @@ document.addEventListener("DOMContentLoaded",()=>{
             span.parentElement.classList.toggle("on")
         })
     })
-
-})
+}
