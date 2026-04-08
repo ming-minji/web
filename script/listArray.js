fdcda6088da630c51b5c7e6f5ca82f94534fe205
김민지 <mando304010@gmail.com>
2026-04-08 17:45:49 +0900

    사진오류 해결

--- script/listArray.js
+++ script/listArray.js
@@ -4,7 +4,7 @@ let listArray = [
         pName: "Grilled Sausage",
         pDesc: "그릴에서 노릇하게 구워 육즙이 톡 터지는 소세지, 든든한 에너지가 필요한 당신을 위한 완벽한 선택",
         pPrice: 6500,
-        pThumnail: "sand-grilled.jpg",
+        pThumbnail: "sand-grilled.jpg",
         plike:false
     },
     {
@@ -12,7 +12,7 @@ let listArray = [
         pName: "Sweet Corn Mayo",
         pDesc: "톡톡 터지는 옥수수와 특제 마요 소스의 달콤 짭짤한 조화, 기분 좋은 활력을 주는 마성의 맛",
         pPrice: 5200,
-        pThumnail: "sand-corn.jpg",
+        pThumbnail: "sand-corn.jpg",
         plike:false
     },
     {
@@ -20,7 +20,7 @@ let listArray = [
         pName: "Shrimp Egg Salad",
         pDesc: "탱글한 새우와 부드러운 스크램블 에그의 만남, 입안 가득 피어나는 고소하고 담백한 풍미",
         pPrice: 6800,
-        pThumnail: "sand-shrimp.jpg",
+        pThumbnail: "sand-shrimp.jpg",
         plike:false
     },
     {
@@ -28,7 +28,7 @@ let listArray = [
         pName: "Bulgogi Cheese Melt",
         pDesc: "입안 가득 터지는 달콤 짭짤 불고기와 부드러운 치즈의 완벽한 하모니, 누구나 사랑하는 든든하고 익숙한 행복의 맛",
         pPrice: 7200,
-        pThumnail: "sand-bulgogi.jpg",
+        pThumbnail: "sand-bulgogi.jpg",
         plike:false
     },
     {
@@ -36,7 +36,7 @@ let listArray = [
         pName: "Maple Bacon B.L.T",
         pDesc: "달콤한 메이플 베이컨과 신선한 야채의 아삭한 조화, 샌디의 정석 클래식",
         pPrice: 5500,
-        pThumnail: "sand-blt.jpg",
+        pThumbnail: "sand-blt.jpg",
         plike:false
     },
     {
@@ -44,7 +44,7 @@ let listArray = [
         pName: "Mild Chicken Breast",
         pDesc: "담백한 닭가슴살과 신선한 채소로 가볍지만 든든하게 채우는 에너지",
         pPrice: 6200,
-        pThumnail: "sand-chicken.jpg",
+        pThumbnail: "sand-chicken.jpg",
         plike:false
     },
     {
@@ -52,7 +52,7 @@ let listArray = [
         pName: "Spicy Chicken Melt",
         pDesc: "매콤한 닭다리살과 진한 치즈의 화끈한 만남, 스트레스를 날려줄 풍미",
         pPrice: 7500,
-        pThumnail: "sand-spicy.jpg",
+        pThumbnail: "sand-spicy.jpg",
         plike:false
     },
     {
@@ -60,7 +60,7 @@ let listArray = [
         pName: "Steak & Mushroom",
         pDesc: "육즙 가득 스테이크와 볶은 버섯의 깊은 불맛을 담은 프리미엄 멜트",
         pPrice: 7800,
-        pThumnail: "sand-steak.jpg",
+        pThumbnail: "sand-steak.jpg",
         plike:false
     }
 ]
